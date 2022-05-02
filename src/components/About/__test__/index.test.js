6cd81cfdb2167d8ffda877da7b48bb532d9c8f43
Zach Davis <zacharydavis92@gmail.com>
2022-05-02 13:05:11 -0500

    'added first test'

---
+++ src/components/About/__test__/index.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import About from "..";
+
+afterEach(cleanup);
+
+describe('About components', () => {
+    // First test
+    // used 'it' function. The first argument, a string declares what is being tested. The second argument, a callback function, runs the test.
+    it('renders', () => {
+        render(<About />);
+    });
+
+    // Second test
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+    })
+})
